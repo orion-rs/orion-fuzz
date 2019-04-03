b7586ba4a3a07ae4a8bed891e5020e53b261af15
brycx <brycx@protonmail.com>
2019-04-03 13:00:41 +0200

    kdf: Fuzz target function for pbkdf2. Remove verify() call as it adds too much workload

--- src/kdf.rs
+++ src/kdf.rs
@@ -5,7 +5,7 @@ extern crate ring;
 pub mod utils;
 
 use orion::hazardous::constants::SHA512_OUTSIZE;
-use orion::hazardous::kdf::hkdf;
+use orion::hazardous::kdf::{hkdf, pbkdf2};
 use utils::{make_seeded_rng, ChaChaRng, RngCore};
 
 fn fuzz_hkdf(fuzzer_input: &[u8], seeded_rng: &mut ChaChaRng) {
@@ -47,21 +47,45 @@ fn fuzz_hkdf(fuzzer_input: &[u8], seeded_rng: &mut ChaChaRng) {
     hkdf::derive_key(&salt, &ikm, Some(&info), &mut orion_okm).unwrap();
     ring::hkdf::extract_and_expand(&other_salt, &ikm, &info, &mut other_okm);
     assert_eq!(orion_okm, other_okm);
-    
-    // Verify the OKM of both orion and ring
-    let mut orion_verify_itself_out = orion_okm.clone();
-    let mut orion_verify_other_out = orion_okm.clone();
-    assert!(hkdf::verify(&orion_okm, &salt, &ikm, Some(&info), &mut orion_verify_itself_out).unwrap());
-    assert!(hkdf::verify(&other_okm, &salt, &ikm, Some(&info), &mut orion_verify_other_out).unwrap());
 }
 
 fn fuzz_pbkdf2(fuzzer_input: &[u8], seeded_rng: &mut ChaChaRng) {
+    let mut password = vec![0u8; fuzzer_input.len() / 2];
+    seeded_rng.fill_bytes(&mut password);
 
+    let mut salt = vec![0u8; fuzzer_input.len() / 4];
+    seeded_rng.fill_bytes(&mut salt);
 
+    // Cast to u16 so we don't have too many blocks to process.
+    let dk_length = seeded_rng.next_u32() as u16;
 
+    let mut orion_dk: Vec<u8> = if dk_length == 0 {
+        vec![0u8; 64]
+    } else {
+        vec![0u8; dk_length as usize]
+    };
 
+    let mut other_dk = orion_dk.clone();
+    // Cast to u16 so we don't have too many iterations.
+    let mut iterations = seeded_rng.next_u32() as u16;
+    if iterations == 0 {
+        iterations = 1;
+    }
 
+    // orion
+    let orion_password = pbkdf2::Password::from_slice(&password).unwrap();
+    pbkdf2::derive_key(&orion_password, &salt, iterations as usize, &mut orion_dk).unwrap();
 
+    // ring
+    ring::pbkdf2::derive(
+        &ring::digest::SHA512,
+        std::num::NonZeroU32::new(u32::from(iterations)).unwrap(),
+        &salt,
+        &password,
+        &mut other_dk,
+    );
+
+    assert_eq!(orion_dk, other_dk);
 }
 
 fn main() {
@@ -73,7 +97,7 @@ fn main() {
             // Test `orion::hazardous::kdf::hkdf`
             fuzz_hkdf(data, &mut seeded_rng);
             // Test `orion::hazardous::kdf::pbkdf2`
-            // fuzz_pbkdf2(data, &mut seeded_rng);
+            fuzz_pbkdf2(data, &mut seeded_rng);
         });
     }
 }
