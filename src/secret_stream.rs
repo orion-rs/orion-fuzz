9bf8b67102cb6b011657f529de508570e5bdfc86
brycx <brycx@protonmail.com>
2019-10-06 12:37:58 +0200

    scret_stream: Add sodiumoxide comparison and update new orion API

--- src/secret_stream.rs
+++ src/secret_stream.rs
@@ -1,6 +1,7 @@
 #[macro_use]
 extern crate honggfuzz;
 extern crate orion;
+extern crate sodiumoxide;
 pub mod utils;
 
 use utils::{make_seeded_rng, ChaChaRng, RngCore};
@@ -8,17 +9,16 @@ use utils::{make_seeded_rng, ChaChaRng, RngCore};
 use orion::hazardous::secret_stream::xchacha20poly1305::*;
 use orion::hazardous::stream::chacha20::SecretKey;
 
+use sodiumoxide::crypto::secretstream::xchacha20poly1305 as sodium_stream;
+
 /// `orion::hazardous::` // TODO: Missing
 fn fuzz_secret_stream(fuzzer_input: &[u8], seeded_rng: &mut ChaChaRng) {
     if fuzzer_input.is_empty() {
         return;
     }
-    
+
     let mut key = vec![0u8; 32];
     seeded_rng.fill_bytes(&mut key);
-
-    let mut nonce = vec![0u8; 24];
-    seeded_rng.fill_bytes(&mut nonce);
 
     // `ad` will be both tested as Some and None as None is the same as [0u8; 0]
     let ad: Vec<u8> = if fuzzer_input.is_empty() {
@@ -31,51 +31,93 @@ fn fuzz_secret_stream(fuzzer_input: &[u8], seeded_rng: &mut ChaChaRng) {
         vec![0u8; 0]
     };
 
+    let (mut sodium_state_enc, sodium_header) =
+        sodium_stream::Stream::init_push(&sodium_stream::Key::from_slice(&key).unwrap()).unwrap();
+
     let mut orion_state_enc = SecretStreamXChaCha20Poly1305::new(
-        SecretKey::from_slice(&key[..]).unwrap(),
-        Nonce::from_slice(&nonce[..]).unwrap(),
+        &SecretKey::from_slice(&key[..]).unwrap(),
+        &Nonce::from_slice(sodium_header.as_ref()).unwrap(),
     );
 
     // Push/Encrypt
     let rnd_chunksize = seeded_rng.next_u32() as usize;
     let mut collected_enc: Vec<u8> = Vec::new();
-    
+
     for (idx, input_chunk) in fuzzer_input.chunks(rnd_chunksize).enumerate() {
-        
-        let mut msg: Vec<u8> = vec![0u8; input_chunk.len() + SECRETSTREAM_XCHACHA20POLY1305_ABYTES];
-        
-        if input_chunk.len() < rnd_chunksize || (input_chunk.len() * (idx + 1) == fuzzer_input.len()) {
-			// Last message in the stream
-            orion_state_enc.encrypt_message(input_chunk, &ad, &mut msg, Tag::FINISH).unwrap();
-		} else {
-            orion_state_enc.encrypt_message(input_chunk, &ad, &mut msg, Tag::MESSAGE).unwrap();
+        let mut orion_msg: Vec<u8> =
+            vec![0u8; input_chunk.len() + SECRETSTREAM_XCHACHA20POLY1305_ABYTES];
+        let mut sodium_msg = orion_msg.clone();
+
+        if input_chunk.len() < rnd_chunksize
+            || (input_chunk.len() * (idx + 1) == fuzzer_input.len())
+        {
+            // Last message in the stream
+            orion_state_enc
+                .encrypt_message(input_chunk, Some(&ad), &mut orion_msg, Tag::FINISH)
+                .unwrap();
+
+            sodium_state_enc
+                .push_to_vec(
+                    input_chunk,
+                    Some(&ad),
+                    sodium_stream::Tag::Final,
+                    &mut sodium_msg,
+                )
+                .unwrap();
+        } else {
+            orion_state_enc
+                .encrypt_message(input_chunk, Some(&ad), &mut orion_msg, Tag::MESSAGE)
+                .unwrap();
+
+            sodium_state_enc
+                .push_to_vec(
+                    input_chunk,
+                    Some(&ad),
+                    sodium_stream::Tag::Message,
+                    &mut sodium_msg,
+                )
+                .unwrap();
         }
 
-        collected_enc.extend_from_slice(&msg);
+        assert_eq!(orion_msg, sodium_msg);
+        collected_enc.extend_from_slice(&orion_msg);
     }
 
     let mut orion_state_dec = SecretStreamXChaCha20Poly1305::new(
-        SecretKey::from_slice(&key[..]).unwrap(),
-        Nonce::from_slice(&nonce[..]).unwrap(),
+        &SecretKey::from_slice(&key[..]).unwrap(),
+        &Nonce::from_slice(sodium_header.as_ref()).unwrap(),
     );
+
+    let mut sodium_state_dec = sodium_stream::Stream::init_pull(
+        &sodium_header,
+        &sodium_stream::Key::from_slice(&key).unwrap(),
+    )
+    .unwrap();
 
     let mut collected_dec: Vec<u8> = Vec::new();
     let dec_rnd_chunksize = rnd_chunksize + SECRETSTREAM_XCHACHA20POLY1305_ABYTES;
 
     for (idx, input_chunk) in collected_enc.chunks(dec_rnd_chunksize).enumerate() {
-        let mut tag = Tag::empty();
-        let mut msg: Vec<u8> = vec![0u8; input_chunk.len() - SECRETSTREAM_XCHACHA20POLY1305_ABYTES];
-        
-        orion_state_dec.decrypt_message(input_chunk, &ad, &mut msg, &mut tag).unwrap();
+        let mut orion_msg: Vec<u8> =
+            vec![0u8; input_chunk.len() - SECRETSTREAM_XCHACHA20POLY1305_ABYTES];
 
-        if input_chunk.len() < dec_rnd_chunksize || (input_chunk.len() * (idx + 1) == collected_enc.len()) {
-			// Last message in the stream
-            assert_eq!(tag, Tag::FINISH);
-		} else {
-            assert_eq!(tag, Tag::MESSAGE);
+        let orion_tag = orion_state_dec
+            .decrypt_message(input_chunk, Some(&ad), &mut orion_msg)
+            .unwrap();
+
+        let (sodium_msg, _sodium_tag) = sodium_state_dec.pull(input_chunk, Some(&ad)).unwrap();
+
+        if input_chunk.len() < dec_rnd_chunksize
+            || (input_chunk.len() * (idx + 1) == collected_enc.len())
+        {
+            // Last message in the stream
+            assert_eq!(orion_tag, Tag::FINISH);
+        } else {
+            assert_eq!(orion_tag, Tag::MESSAGE);
         }
 
-        collected_dec.extend_from_slice(&msg);
+        assert_eq!(orion_msg, sodium_msg);
+        collected_dec.extend_from_slice(&orion_msg);
     }
 
     assert_eq!(fuzzer_input, &collected_dec[..]);
