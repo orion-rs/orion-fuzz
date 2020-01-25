899d20e1b2853d38b35d653b30b30ae26fef8ced
brycx <brycx@protonmail.com>
2020-01-25 12:49:29 +0100

    Refactor kdf fuzzer

--- src/kdf.rs
+++ src/kdf.rs
@@ -6,12 +6,10 @@ extern crate ring;
 pub mod utils;
 
 use argon2::{Config, ThreadMode, Variant, Version};
-use orion::hazardous::hash::sha512::SHA512_OUTSIZE;
 use orion::hazardous::kdf::{argon2 as orion_argon2, hkdf, pbkdf2};
-use utils::{make_seeded_rng, rand_in_range, ChaChaRng, RngCore};
+use utils::{make_seeded_rng, rand_vec_in_range, ChaChaRng, Rng};
 
 /// See: https://github.com/briansmith/ring/blob/master/tests/hkdf_tests.rs
-
 /// Generic newtype wrapper that lets us implement traits for externally-defined
 /// types.
 struct RingHkdf<T>(T);
@@ -31,34 +29,18 @@ impl From<ring::hkdf::Okm<'_, RingHkdf<usize>>> for RingHkdf<Vec<u8>> {
 }
 
 fn fuzz_hkdf(fuzzer_input: &[u8], seeded_rng: &mut ChaChaRng) {
-    let mut ikm = vec![0u8; fuzzer_input.len() / 2];
-    seeded_rng.fill_bytes(&mut ikm);
+    let outsize: usize = seeded_rng.gen_range(1, 16320 + 1);
 
-    let mut salt = vec![0u8; fuzzer_input.len() / 4];
-    seeded_rng.fill_bytes(&mut salt);
-
-    let mut orion_okm: Vec<u8> =
-        if (fuzzer_input.len() / 2) > (255 * SHA512_OUTSIZE) || (fuzzer_input.len() / 2) < 1 {
-            vec![0u8; 256]
-        } else {
-            vec![0u8; fuzzer_input.len() / 2]
-        };
-
-    // Empty info will be the same as None.
-    let info: Vec<u8> = if fuzzer_input.is_empty() {
-        vec![0u8; 0]
-    } else {
-        vec![0u8; fuzzer_input[0] as usize]
-    };
+    let ikm = fuzzer_input;
+    let salt = rand_vec_in_range(seeded_rng, 0, 128);
+    let info = rand_vec_in_range(seeded_rng, 0, 128);
+    let mut orion_okm = vec![0u8; outsize];
 
     // orion
-    let orion_prk = hkdf::extract(&salt, &ikm).unwrap();
-    hkdf::expand(&orion_prk, Some(&info), &mut orion_okm).unwrap();
+    hkdf::derive_key(&salt, &ikm, Some(&info), &mut orion_okm).unwrap();
 
     // ring
     let other_salt = ring::hkdf::Salt::new(ring::hkdf::HKDF_SHA512, &salt);
-
-    // See: https://github.com/briansmith/ring/blob/master/tests/hkdf_tests.rs
     let RingHkdf(other_okm) = other_salt
         .extract(&ikm)
         .expand(&[&info], RingHkdf(orion_okm.len()))
@@ -66,33 +48,16 @@ fn fuzz_hkdf(fuzzer_input: &[u8], seeded_rng: &mut ChaChaRng) {
         .into();
 
     assert_eq!(orion_okm, other_okm);
-    // Test extract-then-expand combination
-    hkdf::derive_key(&salt, &ikm, Some(&info), &mut orion_okm).unwrap();
-    assert_eq!(orion_okm, other_okm);
 }
 
 fn fuzz_pbkdf2(fuzzer_input: &[u8], seeded_rng: &mut ChaChaRng) {
-    let mut password = vec![0u8; fuzzer_input.len() / 2];
-    seeded_rng.fill_bytes(&mut password);
+    let outsize: usize = seeded_rng.gen_range(1, 256 + 1);
+    let iterations: u32 = seeded_rng.gen_range(1, 1000 + 1);
 
-    let mut salt = vec![0u8; fuzzer_input.len() / 4];
-    seeded_rng.fill_bytes(&mut salt);
-
-    // Cast to u16 so we don't have too many blocks to process.
-    let dk_length = seeded_rng.next_u32() as u16;
-
-    let mut orion_dk: Vec<u8> = if dk_length == 0 {
-        vec![0u8; 64]
-    } else {
-        vec![0u8; dk_length as usize]
-    };
-
-    let mut other_dk = orion_dk.clone();
-    // Cast to u16 so we don't have too many iterations.
-    let mut iterations = seeded_rng.next_u32() as u16;
-    if iterations == 0 {
-        iterations = 1;
-    }
+    let password = fuzzer_input;
+    let salt = rand_vec_in_range(seeded_rng, 0, 128);
+    let mut orion_dk = vec![0u8; outsize];
+    let mut other_dk = vec![0u8; outsize];
 
     // orion
     let orion_password = pbkdf2::Password::from_slice(&password).unwrap();
@@ -101,7 +66,7 @@ fn fuzz_pbkdf2(fuzzer_input: &[u8], seeded_rng: &mut ChaChaRng) {
     // ring
     ring::pbkdf2::derive(
         ring::pbkdf2::PBKDF2_HMAC_SHA512,
-        std::num::NonZeroU32::new(u32::from(iterations)).unwrap(),
+        std::num::NonZeroU32::new(iterations).unwrap(),
         &salt,
         &password,
         &mut other_dk,
@@ -112,56 +77,44 @@ fn fuzz_pbkdf2(fuzzer_input: &[u8], seeded_rng: &mut ChaChaRng) {
 
 fn fuzz_argon2(fuzzer_input: &[u8], seeded_rng: &mut ChaChaRng) {
     let lanes = 1;
+    let outsize: u32 = seeded_rng.gen_range(4, 256 + 1);
+    let memory: u32 = seeded_rng.gen_range(8, 1024 + 1);
+    let passes: u32 = seeded_rng.gen_range(1, 10 + 1);
 
     let password = fuzzer_input;
+    let salt = rand_vec_in_range(seeded_rng, 8, 32);
+    let secret = rand_vec_in_range(seeded_rng, 0, 32);
+    let ad = rand_vec_in_range(seeded_rng, 0, 32);
 
-    let mut salt = vec![0u8; 16];
-    seeded_rng.fill_bytes(&mut salt);
-
-    let mut secret_value = vec![0u8; 16];
-    seeded_rng.fill_bytes(&mut secret_value);
-
-    let mut associated_data = vec![0u8; 16];
-    seeded_rng.fill_bytes(&mut associated_data);
-
-    let outsize: u32 = if fuzzer_input.len() >= 4 {
-        fuzzer_input.len() as u32
-    } else {
-        32
-    };
-
-    let mem = rand_in_range(seeded_rng, 8, 8192);
-    let passes = 3;
-
-    // Other
+    // rust-argon2
     let config = Config {
         variant: Variant::Argon2i,
         version: Version::Version13,
-        mem_cost: mem,
+        mem_cost: memory,
         time_cost: passes,
         lanes,
         thread_mode: ThreadMode::Sequential,
-        secret: &secret_value,
-        ad: &associated_data,
+        secret: &secret,
+        ad: &ad,
         hash_length: outsize,
     };
 
-    let other_hash = argon2::hash_raw(&password[..], &salt[..], &config).unwrap();
+    let other_dk = argon2::hash_raw(&password[..], &salt[..], &config).unwrap();
 
-    // Orion
-    let mut dst_out = vec![0u8; outsize as usize];
+    // orion
+    let mut orion_dk = vec![0u8; outsize as usize];
     orion_argon2::derive_key(
         &password,
         &salt,
         passes,
-        mem,
-        Some(&secret_value),
-        Some(&associated_data),
-        &mut dst_out,
+        memory,
+        Some(&secret),
+        Some(&ad),
+        &mut orion_dk,
     )
     .unwrap();
 
-    assert_eq!(other_hash, dst_out);
+    assert_eq!(other_dk, orion_dk);
 }
 
 fn main() {
--- src/utils.rs
+++ src/utils.rs
@@ -1,8 +1,8 @@
+extern crate rand;
 extern crate rand_chacha;
-extern crate rand_core;
 
+pub use self::rand::{Rng, RngCore, SeedableRng};
 pub use self::rand_chacha::ChaChaRng;
-pub use self::rand_core::{RngCore, SeedableRng};
 
 pub fn make_seeded_rng(fuzzer_input: &[u8]) -> ChaChaRng {
     // We need 8 bytes worth of data to convet into u64, so start with zero and replace
@@ -19,13 +19,11 @@ pub fn make_seeded_rng(fuzzer_input: &[u8]) -> ChaChaRng {
     ChaChaRng::seed_from_u64(seed)
 }
 
-/// Generate random u32 within the lower and upper bound (inclusive).
-pub fn rand_in_range(rng: &mut ChaChaRng, lb: u32, ub: u32) -> u32 {
-    let mut res = rng.next_u32();
+/// Generate a vector of random length within the lower and upper bound (both inclusive) and fill it with random data.
+pub fn rand_vec_in_range(seeded_rng: &mut ChaChaRng, lb: usize, ub: usize) -> Vec<u8> {
+    let rand_len: usize = seeded_rng.gen_range(lb, ub + 1);
+    let mut bytes = vec![0u8; rand_len];
+    seeded_rng.fill_bytes(&mut bytes);
 
-    while res < lb || res > ub {
-        res = rng.next_u32();
-    }
-
-    res
+    bytes
 }
