f173f4e68f083edf27358113138f78a60adb5045
brycx <brycx@protonmail.com>
2021-10-17 11:50:58 +0200

    ecc: Update fuzzing target

--- src/ecc.rs
+++ src/ecc.rs
@@ -4,7 +4,7 @@ extern crate orion;
 extern crate x25519_dalek;
 
 use orion::hazardous::ecc::x25519;
-use std::convert::{TryFrom, TryInto};
+use std::convert::TryFrom;
 use utils::{make_seeded_rng, ChaChaRng, RngCore};
 
 pub mod utils;
@@ -12,14 +12,14 @@ pub mod utils;
 /// `orion::hazardous::ecc::x25519`
 fn fuzz_x25519(seeded_rng: &mut ChaChaRng) {
     // Key-agreement
-    let mut alice_k = [0u8; x25519::SECRET_KEY_SIZE];
-    let mut bob_k = [0u8; x25519::SECRET_KEY_SIZE];
+    let mut alice_k = [0u8; x25519::PRIVATE_KEY_SIZE];
+    let mut bob_k = [0u8; x25519::PRIVATE_KEY_SIZE];
     seeded_rng.fill_bytes(&mut alice_k);
     seeded_rng.fill_bytes(&mut bob_k);
 
-    let alice_secret = x25519::SecretKey::from_slice(&alice_k).unwrap();
+    let alice_secret = x25519::PrivateKey::from_slice(&alice_k).unwrap();
     let alice_public = x25519::PublicKey::try_from(&alice_secret).unwrap();
-    let bob_secret = x25519::SecretKey::from_slice(&bob_k).unwrap();
+    let bob_secret = x25519::PrivateKey::from_slice(&bob_k).unwrap();
     let bob_public = x25519::PublicKey::try_from(&bob_secret).unwrap();
 
     let alice_shared = x25519::key_agreement(&alice_secret, &bob_public).unwrap();
@@ -28,8 +28,8 @@ fn fuzz_x25519(seeded_rng: &mut ChaChaRng) {
     assert_eq!(alice_shared, bob_shared);
 
     // x25519_dalek (we use the bare-byte function since this is the one documented as adherent to RFC)
-    let dalek_alice_public: [u8; 32] = alice_public.as_ref().try_into().unwrap();
-    let dalek_bob_public: [u8; 32] = bob_public.as_ref().try_into().unwrap();
+    let dalek_alice_public: [u8; 32] = alice_public.to_bytes();
+    let dalek_bob_public: [u8; 32] = bob_public.to_bytes();
     let dalek_alice_shared = x25519_dalek::x25519(alice_k, dalek_bob_public);
     let dalek_bob_shared = x25519_dalek::x25519(bob_k, dalek_alice_public);
 
