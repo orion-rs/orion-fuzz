d8f7d745c982e143d270205f8df6da750ed200c2
brycx <brycx@protonmail.com>
2021-10-26 08:46:10 +0200

    ecc: Fuzzer for high-level kex module

--- src/ecc.rs
+++ src/ecc.rs
@@ -1,6 +1,7 @@
 #[macro_use]
 extern crate honggfuzz;
 extern crate orion;
+extern crate sodiumoxide;
 extern crate x25519_dalek;
 
 use orion::hazardous::ecc::x25519;
@@ -37,6 +38,72 @@ fn fuzz_x25519(seeded_rng: &mut ChaChaRng) {
     assert_eq!(bob_shared, dalek_bob_shared.as_ref());
 }
 
+/// TODO: Move into high-level module? Those don't have differential fuzzers but this is a high-level API.
+/// `orion::kex:`
+fn fuzz_kex() {
+    use orion::kex;
+    use sodiumoxide::crypto::kx;
+
+    // orion - keys
+    let client_session = kex::EphemeralClientSession::new().unwrap();
+    let client_public_key = client_session.get_public();
+    let server_session = kex::EphemeralServerSession::new().unwrap();
+    let server_public_key = server_session.get_public();
+
+    // sodiumoxide - keys
+    let client_sk = kx::SecretKey::from_slice(
+        client_session
+            .unprotected_private_key()
+            .unprotected_as_bytes(),
+    )
+    .unwrap();
+    let client_pk = kx::PublicKey::from_slice(&client_public_key.to_bytes()).unwrap();
+    let server_sk = kx::SecretKey::from_slice(
+        server_session
+            .unprotected_private_key()
+            .unprotected_as_bytes(),
+    )
+    .unwrap();
+    let server_pk = kx::PublicKey::from_slice(&server_public_key.to_bytes()).unwrap();
+
+    // sodiumoxide - key exchange
+    let (client_recv, client_trans) =
+        match kx::client_session_keys(&client_pk, &client_sk, &server_pk) {
+            Ok((rx, tx)) => (rx, tx),
+            Err(()) => panic!("bad server signature"),
+        };
+    let (server_recv, server_trans) =
+        match kx::server_session_keys(&server_pk, &server_sk, &client_pk) {
+            Ok((rx, tx)) => (rx, tx),
+            Err(()) => panic!("bad client signature"),
+        };
+
+    // orion - key exchange
+    let client_shared = client_session
+        .establish_with_server(&server_public_key)
+        .unwrap();
+    let server_shared = server_session
+        .establish_with_client(&client_public_key)
+        .unwrap();
+
+    assert_eq!(
+        client_shared.get_receiving().unprotected_as_bytes(),
+        client_recv.as_ref()
+    );
+    assert_eq!(
+        client_shared.get_transport().unprotected_as_bytes(),
+        client_trans.as_ref()
+    );
+    assert_eq!(
+        server_shared.get_receiving().unprotected_as_bytes(),
+        server_recv.as_ref()
+    );
+    assert_eq!(
+        server_shared.get_transport().unprotected_as_bytes(),
+        server_trans.as_ref()
+    );
+}
+
 fn main() {
     loop {
         fuzz!(|data: &[u8]| {
@@ -45,6 +112,7 @@ fn main() {
 
             // Test `orion::hazardous::ecc::x25519`
             fuzz_x25519(&mut seeded_rng);
+            fuzz_kex();
         });
     }
 }
