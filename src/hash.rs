a1b4c87ebec6990bc269739079b27d76085ebee3
Johannes <brycx@protonmail.com>
2023-06-19 08:13:04 +0200

    Implement differential fuzzing target for SHA3 (#20)
    * hash: Implement Sha2FuzzType for SHA2/3 via macro
    
    * sha3: Add differential fuzzing target for SHA3
    
    * hash: New Sha3 naming
    
    * Change to  branch for Orion

--- src/hash.rs
+++ src/hash.rs
@@ -5,7 +5,25 @@ extern crate orion;
 extern crate ring;
 pub mod utils;
 
-use orion::hazardous::hash::sha2;
+use sha3 as other_sha3;
+
+use orion::hazardous::hash::sha2::sha256::{Digest as Sha256Digest, Sha256, SHA256_BLOCKSIZE};
+use orion::hazardous::hash::sha2::sha384::{Digest as Sha384Digest, Sha384, SHA384_BLOCKSIZE};
+use orion::hazardous::hash::sha2::sha512::{Digest as Sha512Digest, Sha512, SHA512_BLOCKSIZE};
+
+use orion::hazardous::hash::sha3::sha3_224::{
+    Digest as Sha3_224Digest, Sha3_224, SHA3_224_RATE,
+};
+use orion::hazardous::hash::sha3::sha3_256::{
+    Digest as Sha3_256Digest, Sha3_256, SHA3_256_RATE,
+};
+use orion::hazardous::hash::sha3::sha3_384::{
+    Digest as Sha3_384Digest, Sha3_384, SHA3_384_RATE,
+};
+use orion::hazardous::hash::sha3::sha3_512::{
+    Digest as Sha3_512Digest, Sha3_512, SHA3_512_RATE,
+};
+
 use orion::{errors::UnknownCryptoError, hazardous::hash::blake2::blake2b};
 use std::marker::PhantomData;
 use utils::{make_seeded_rng, ChaChaRng, Rng};
@@ -62,7 +80,7 @@ fn fuzz_blake2b(fuzzer_input: &[u8], seeded_rng: &mut ChaChaRng) {
 }
 
 // A wrapper trait to reduce duplicate functional test-code when fuzzing SHA256/384/512.
-trait Sha2FuzzType<T: PartialEq> {
+trait ShaFuzzType<T: PartialEq> {
     fn reset(&mut self);
 
     fn update(&mut self, data: &[u8]) -> Result<(), UnknownCryptoError>;
@@ -74,69 +92,92 @@ trait Sha2FuzzType<T: PartialEq> {
     fn get_blocksize() -> usize;
 }
 
-impl Sha2FuzzType<sha2::sha256::Digest> for sha2::sha256::Sha256 {
-    fn reset(&mut self) {
-        self.reset();
-    }
+/// A trait for the other implementation that Orion should be fuzzed against.
+/// This is used for SHA3 as it's not compatible with the apporahc used for SHA2.
+trait ShaComparableType<T>
+where
+    T: AsRef<[u8]>,
+{
+    fn digest(data: &[u8]) -> T;
+}
 
-    fn update(&mut self, data: &[u8]) -> Result<(), UnknownCryptoError> {
-        self.update(data)
-    }
+macro_rules! impl_sha_fuzztype_trait (($sha_variant:ident, $sha_digest:ident, $sha_bs:expr) => (
+    impl ShaFuzzType<$sha_digest> for $sha_variant {
+        fn reset(&mut self) {
+            self.reset();
+        }
 
-    fn finalize(&mut self) -> Result<sha2::sha256::Digest, UnknownCryptoError> {
-        self.finalize()
-    }
+        fn update(&mut self, data: &[u8]) -> Result<(), UnknownCryptoError> {
+            self.update(data)
+        }
 
-    fn digest(&self, data: &[u8]) -> Result<sha2::sha256::Digest, UnknownCryptoError> {
-        sha2::sha256::Sha256::digest(data)
-    }
+        fn finalize(&mut self) -> Result<$sha_digest, UnknownCryptoError> {
+            self.finalize()
+        }
 
-    fn get_blocksize() -> usize {
-        sha2::sha256::SHA256_BLOCKSIZE
+        fn digest(&self, data: &[u8]) -> Result<$sha_digest, UnknownCryptoError> {
+            $sha_variant::digest(data)
+        }
+
+        fn get_blocksize() -> usize {
+            $sha_bs
+        }
+    }
+));
+
+impl_sha_fuzztype_trait!(Sha256, Sha256Digest, SHA256_BLOCKSIZE);
+impl_sha_fuzztype_trait!(Sha384, Sha384Digest, SHA384_BLOCKSIZE);
+impl_sha_fuzztype_trait!(Sha512, Sha512Digest, SHA512_BLOCKSIZE);
+
+impl_sha_fuzztype_trait!(Sha3_224, Sha3_224Digest, SHA3_224_RATE);
+impl_sha_fuzztype_trait!(Sha3_256, Sha3_256Digest, SHA3_256_RATE);
+impl_sha_fuzztype_trait!(Sha3_384, Sha3_384Digest, SHA3_384_RATE);
+impl_sha_fuzztype_trait!(Sha3_512, Sha3_512Digest, SHA3_512_RATE);
+
+// We use Orion's return Digest here otherwise we get too many generic type
+// parameters. So we construct Orion's Digest from `sha3` crate and compare later.
+
+impl ShaComparableType<Sha3_224Digest> for other_sha3::Sha3_224 {
+    fn digest(data: &[u8]) -> Sha3_224Digest {
+        use sha3::Digest;
+        let mut hasher = sha3::Sha3_224::new();
+        hasher.update(data);
+        let hash = hasher.finalize();
+
+        Sha3_224Digest::from_slice(hash.as_ref()).unwrap()
     }
 }
 
-impl Sha2FuzzType<sha2::sha384::Digest> for sha2::sha384::Sha384 {
-    fn reset(&mut self) {
-        self.reset();
-    }
+impl ShaComparableType<Sha3_256Digest> for other_sha3::Sha3_256 {
+    fn digest(data: &[u8]) -> Sha3_256Digest {
+        use sha3::Digest;
+        let mut hasher = sha3::Sha3_256::new();
+        hasher.update(data);
+        let hash = hasher.finalize();
 
-    fn update(&mut self, data: &[u8]) -> Result<(), UnknownCryptoError> {
-        self.update(data)
-    }
-
-    fn finalize(&mut self) -> Result<sha2::sha384::Digest, UnknownCryptoError> {
-        self.finalize()
-    }
-
-    fn digest(&self, data: &[u8]) -> Result<sha2::sha384::Digest, UnknownCryptoError> {
-        sha2::sha384::Sha384::digest(data)
-    }
-
-    fn get_blocksize() -> usize {
-        sha2::sha384::SHA384_BLOCKSIZE
+        Sha3_256Digest::from_slice(hash.as_ref()).unwrap()
     }
 }
 
-impl Sha2FuzzType<sha2::sha512::Digest> for sha2::sha512::Sha512 {
-    fn reset(&mut self) {
-        self.reset();
-    }
+impl ShaComparableType<Sha3_384Digest> for other_sha3::Sha3_384 {
+    fn digest(data: &[u8]) -> Sha3_384Digest {
+        use sha3::Digest;
+        let mut hasher = sha3::Sha3_384::new();
+        hasher.update(data);
+        let hash = hasher.finalize();
 
-    fn update(&mut self, data: &[u8]) -> Result<(), UnknownCryptoError> {
-        self.update(data)
+        Sha3_384Digest::from_slice(hash.as_ref()).unwrap()
     }
+}
 
-    fn finalize(&mut self) -> Result<sha2::sha512::Digest, UnknownCryptoError> {
-        self.finalize()
-    }
+impl ShaComparableType<Sha3_512Digest> for other_sha3::Sha3_512 {
+    fn digest(data: &[u8]) -> Sha3_512Digest {
+        use sha3::Digest;
+        let mut hasher = sha3::Sha3_512::new();
+        hasher.update(data);
+        let hash = hasher.finalize();
 
-    fn digest(&self, data: &[u8]) -> Result<sha2::sha512::Digest, UnknownCryptoError> {
-        sha2::sha512::Sha512::digest(data)
-    }
-
-    fn get_blocksize() -> usize {
-        sha2::sha512::SHA512_BLOCKSIZE
+        Sha3_512Digest::from_slice(hash.as_ref()).unwrap()
     }
 }
 
@@ -152,7 +193,7 @@ struct Sha2Fuzzer<R, T> {
 impl<R, T> Sha2Fuzzer<R, T>
 where
     R: PartialEq + AsRef<[u8]>,
-    T: Sha2FuzzType<R>,
+    T: ShaFuzzType<R>,
 {
     pub fn new(sha2_initial_state: T, ring_digest: &'static ring::digest::Algorithm) -> Self {
         Self {
@@ -202,11 +243,83 @@ where
     }
 }
 
+/// A SHA3 fuzzer.
+struct Sha3Fuzzer<R, T, O> {
+    _return_type: PhantomData<R>,
+    // The initial context to base further calls upon.
+    own_context: T,
+    _other_impl: PhantomData<O>,
+}
+
+impl<R, T, O> Sha3Fuzzer<R, T, O>
+where
+    R: PartialEq + AsRef<[u8]>,
+    T: ShaFuzzType<R>,
+    O: ShaComparableType<R>,
+{
+    pub fn new(sha2_initial_state: T) -> Self {
+        Self {
+            _return_type: PhantomData,
+            own_context: sha2_initial_state,
+            _other_impl: PhantomData,
+        }
+    }
+
+    /// Fuzz the Orion implementation and check results with RustCrypto's `sha3` crate.
+    pub fn fuzz(&mut self, fuzzer_input: &[u8]) {
+        // Clear the state
+        self.own_context.reset();
+
+        let mut collected_data: Vec<u8> = Vec::new();
+
+        collected_data.extend_from_slice(fuzzer_input);
+        self.own_context.update(fuzzer_input).unwrap();
+
+        if fuzzer_input.len() > T::get_blocksize() {
+            collected_data.extend_from_slice(b"");
+            self.own_context.update(b"").unwrap();
+        }
+        if fuzzer_input.len() > T::get_blocksize() * 2 {
+            collected_data.extend_from_slice(b"Extra");
+            self.own_context.update(b"Extra").unwrap();
+        }
+        if fuzzer_input.len() > T::get_blocksize() * 3 {
+            collected_data.extend_from_slice(&[0u8; 256]);
+            self.own_context.update(&[0u8; 256]).unwrap();
+        }
+        if fuzzer_input.len() > T::get_blocksize() * 4 {
+            collected_data.extend_from_slice(&vec![0u8; T::get_blocksize() - 1]);
+            self.own_context
+                .update(&vec![0u8; T::get_blocksize() - 1])
+                .unwrap();
+        }
+
+        let digest_other = O::digest(&collected_data);
+        let orion_one_shot = self.own_context.digest(&collected_data).unwrap();
+
+        assert_eq!(orion_one_shot.as_ref(), digest_other.as_ref());
+        assert_eq!(
+            self.own_context.finalize().unwrap().as_ref(),
+            digest_other.as_ref()
+        );
+    }
+}
+
 fn main() {
     // Setup SHA2
-    let mut sha256_fuzzer = Sha2Fuzzer::new(sha2::sha256::Sha256::new(), &ring::digest::SHA256);
-    let mut sha384_fuzzer = Sha2Fuzzer::new(sha2::sha384::Sha384::new(), &ring::digest::SHA384);
-    let mut sha512_fuzzer = Sha2Fuzzer::new(sha2::sha512::Sha512::new(), &ring::digest::SHA512);
+    let mut sha256_fuzzer = Sha2Fuzzer::new(Sha256::new(), &ring::digest::SHA256);
+    let mut sha384_fuzzer = Sha2Fuzzer::new(Sha384::new(), &ring::digest::SHA384);
+    let mut sha512_fuzzer = Sha2Fuzzer::new(Sha512::new(), &ring::digest::SHA512);
+
+    // Setup SHA3
+    let mut sha3_224_fuzzer: Sha3Fuzzer<Sha3_224Digest, Sha3_224, other_sha3::Sha3_224> =
+        Sha3Fuzzer::new(Sha3_224::new());
+    let mut sha3_256_fuzzer: Sha3Fuzzer<Sha3_256Digest, Sha3_256, other_sha3::Sha3_256> =
+        Sha3Fuzzer::new(Sha3_256::new());
+    let mut sha3_384_fuzzer: Sha3Fuzzer<Sha3_384Digest, Sha3_384, other_sha3::Sha3_384> =
+        Sha3Fuzzer::new(Sha3_384::new());
+    let mut sha3_512_fuzzer: Sha3Fuzzer<Sha3_512Digest, Sha3_512, other_sha3::Sha3_512> =
+        Sha3Fuzzer::new(Sha3_512::new());
 
     loop {
         fuzz!(|data: &[u8]| {
@@ -219,6 +332,11 @@ fn main() {
             sha256_fuzzer.fuzz(data);
             sha384_fuzzer.fuzz(data);
             sha512_fuzzer.fuzz(data);
+            // Test `orion::hazardous::hash::sha3`
+            sha3_224_fuzzer.fuzz(data);
+            sha3_256_fuzzer.fuzz(data);
+            sha3_384_fuzzer.fuzz(data);
+            sha3_512_fuzzer.fuzz(data);
         });
     }
 }
